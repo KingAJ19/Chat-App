2e311c099dc6f97e8314785f7b642f70e79e997b
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-09 14:27:48 +0200

    starting with the form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,7 +4,9 @@ export const Register = () => {
   return (
     <div className='formContainer'>
         <div className="formWrapper">
-            
+            <form>
+                <input type="text" placeholder='Name'></input>
+            </form>
         </div>
     </div>
   )
