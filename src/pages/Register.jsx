590a21dc89ec778c51298c7f20448a6aade382d9
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-11 05:57:01 +0200

    changes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,23 +3,24 @@ import React from 'react'
 export const Register = () => {
   return (
     <div className='formContainer'>
-        <div className="formWrapper">
-            <center>
-            <span className="logo">Super Chat</span> 
-            </center>
-            <br></br>
-            <center>
-            <span className="title">Register</span>
-            </center>
-            <form className='form'>
-                <input type="text" placeholder='Name'></input>
-                <input type="email" placeholder='Email'></input>
-                <input type="password" placeholder='Password'></input>
-                <input type="file" placeholder='Name'></input>
-                <button>Sign Up</button>
-            </form>
-            <p>Have an account? Login</p>
-        </div>
+      <div className="formWrapper">
+        <center>
+          <span className="logo">Super Chat</span>
+
+        </center>
+        <br></br>
+        <center>
+          <span className="title">Register</span>
+        </center>
+        <form className='form'>
+          <input type="text" placeholder='Name'></input>
+          <input type="email" placeholder='Email'></input>
+          <input type="password" placeholder='Password'></input>
+          <input type="file" placeholder='Name'></input>
+          <button>Sign Up</button>
+        </form>
+        <p>Have an account? Login</p>
+      </div>
     </div>
   )
 }
