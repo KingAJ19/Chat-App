1486f6453b49b7314aa8b4df8286dee47644b896
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 13:44:17 +0200

    styling

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='message'>
+    <div className='message owner'>
       <div className="messageInfo">
       <img className='infoImg' src="https://e0.365dm.com/22/05/2048x1152/skysports-steph-curry-nba-basketball_5771331.jpg" alt="" />
       <span>just now</span>
