2a5b15f27783697dca350ad8791603682a129f3b
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-24 01:24:44 +0200

    comment

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDu_Fds2CetA5Y0V_O7dfF7eoBXK_6KXw8",
   authDomain: "chat-6b193.firebaseapp.com",
