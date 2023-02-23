79d1013cf737782e4a1748ae400c5267ddeff0de
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 11:43:39 +0200

    added harry kane

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+      <div className="searchForm">
+        <input type="text" />
+      </div>
+      <div className="userChat">
+        <img className='searchImage' src="https://resources.premierleague.com/premierleague/photo/2023/01/04/effad1fa-ede8-4a56-83d4-d2c840bddf44/Kane-Spurs.jpg" alt="" />
+        <div className="userChatInfo">
+          <span></span>
+        </div>
+      </div>
+    </div>
   )
 }
 
