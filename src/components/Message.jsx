d4bd2a962d4ce117ce9799c7dd53663696b80b64
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 13:29:53 +0200

    typo

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Message = () => {
   return (
     <div className='message'>
-      
+      <div className="messageInfo">
+      <img src="https://e0.365dm.com/22/05/2048x1152/skysports-steph-curry-nba-basketball_5771331.jpg" alt="" />
+      <span>just now</span>
+        <div className="messageContent"></div>
+      </div>
     </div>
   )
 }
