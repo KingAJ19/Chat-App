67bd1b4e6ecc67a930c9b5881d3ca327f85a9f96
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 12:41:49 +0200

    input page

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Input = () => {
   return (
-    <div className='inputC'>Input</div>
+    <div className='inputC'>
+      <input type="text" placeholder='Type Something....'/>
+      <div className="send">
+        <img src="" alt="" />
+        <input type="file" style={{display:"none"}} id="file" />
+        <label htmlFor="file">
+          <img src="" alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
   )
 }
 
