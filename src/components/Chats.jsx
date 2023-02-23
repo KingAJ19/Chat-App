18a629b1e787cd4227ecf4f4282acb747b7fa00b
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 12:02:07 +0200

    kante

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -20,9 +20,9 @@ const Chats = () => {
         </div>
       </div>
       <div className="userChat">
-        <img className='searchImage' src="https://resources.premierleague.com/premierleague/photo/2023/01/04/effad1fa-ede8-4a56-83d4-d2c840bddf44/Kane-Spurs.jpg" alt="" />
+        <img className='searchImage' src="https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blte05a8becf6898cfe/63a0edeb501fcf7ffb82135f/1242501804.jpg?auto=webp&format=jpg&quality=100" alt="" />
         <div className="userChatsInfo">
-          <span>Harry Kane</span>
+          <span>Kante</span>
           <p>Hello</p>
         </div>
       </div>
