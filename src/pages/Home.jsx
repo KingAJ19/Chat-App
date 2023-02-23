a8a4c6e1133bdefd094e8984b47c88ee1ee3421d
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 11:07:11 +0200

    sdide

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
 
 export const Home = () => {
   return (
     <div className='home'>
       <div className="container">
-        
+        <Sidebar />
       </div>
     </div>
   )
