618ddd788be662aae666323debf7b880ad7bfb00
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 13:36:01 +0200

    comment

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,10 +4,11 @@ const Message = () => {
   return (
     <div className='message'>
       <div className="messageInfo">
-      <img src="https://e0.365dm.com/22/05/2048x1152/skysports-steph-curry-nba-basketball_5771331.jpg" alt="" />
+      <img className='infoImg' src="https://e0.365dm.com/22/05/2048x1152/skysports-steph-curry-nba-basketball_5771331.jpg" alt="" />
       <span>just now</span>
         <div className="messageContent">
           <p>hello</p>
+          {/* <img className='contentImg' src="https://s.hdnux.com/photos/01/26/17/63/22609106/7/1200x0.jpg" alt="" /> */}
         </div>
       </div>
     </div>
