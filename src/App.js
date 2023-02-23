286a49f8bc8171d31c33db01af7895d6c87d4f8f
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 10:52:05 +0200

    login is working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Login from "./pages/Login";
 import { Register } from "./pages/Register";
 import './style.css'
 
 function App() {
   return (
-    <Register></Register> 
+    // <Register />
+    <Login />
   );
 }
 
