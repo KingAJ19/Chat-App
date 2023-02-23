58cd5aa802eb4cefe702978ea4eac33a7b58e8b0
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 11:39:29 +0200

    nunez pfp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className='navbar'>
       <span className="navLogo">Super Chat</span>
       <div className="user">
-        <img className='navImage' src="" alt="" />
+        <img className='navImage' src="https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt67ae3fa7e0798570/63dc178bb19823132904002b/Darwin_Nunez_cry_gesture_Liverpool_2022-23.png" alt="" />
         <span>John</span>
         <button className='navBtn'>Log out</button>
       </div>
