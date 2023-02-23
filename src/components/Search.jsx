46d9a080860dffc09bac66b202122b52ef4e3939
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 11:47:17 +0200

    changes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ const Search = () => {
   return (
     <div className='search'>
       <div className="searchForm">
-        <input type="text" />
+        <input type="text" placeholder='Find A User' />
       </div>
       <div className="userChat">
         <img className='searchImage' src="https://resources.premierleague.com/premierleague/photo/2023/01/04/effad1fa-ede8-4a56-83d4-d2c840bddf44/Kane-Spurs.jpg" alt="" />
