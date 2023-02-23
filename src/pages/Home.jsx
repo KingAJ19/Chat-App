ee25405b7d8bf757e836c96d78ce98141ce061cf
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 11:02:02 +0200

    divs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div><h1>HOME</h1></div>
+    <div className='home'>
+      <div className="container">
+        
+      </div>
+    </div>
   )
 }
 
