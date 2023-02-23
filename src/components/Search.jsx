fb968f8c7a3ba71cab51308e0e1b8c148a8bad87
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 11:51:02 +0200

    added Kane

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -9,7 +9,7 @@ const Search = () => {
       <div className="userChat">
         <img className='searchImage' src="https://resources.premierleague.com/premierleague/photo/2023/01/04/effad1fa-ede8-4a56-83d4-d2c840bddf44/Kane-Spurs.jpg" alt="" />
         <div className="userChatInfo">
-          <span></span>
+          <span>Harry Kane</span>
         </div>
       </div>
     </div>
