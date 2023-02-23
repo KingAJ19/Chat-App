82e31bd218ae85c491562e3d09d8026b0848fe47
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 10:49:20 +0200

    copied the code

---
+++ src/pages/Login.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Add from "../img/addAvatar.png"
+
+export const Register = () => {
+  return (
+    <div className='formContainer'>
+      <div className="formWrapper">
+        <center>
+          <span className="logo">Super Chat</span> 
+        </center>
+        <br></br>
+        <center>
+          <span className="title">Register</span>
+        </center>
+        <form className='form'>
+          <input type="text" placeholder='Name'></input>
+          <input type="email" placeholder='Email'></input>
+          <input type="password" placeholder='Password'></input>
+          <input style={{display:"none"}} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="add an avatar" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign Up</button>
+        </form>
+        <p>Have an account? Login</p>
+      </div>
+    </div>
+  )
+} 
+
+
+export default Register
