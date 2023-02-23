217ec7dabd334c013aa7ea49837a43d864479d6b
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 12:29:43 +0200

    fixed the routing

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Cam from '../img/cam.png'
 import Add from '../img/add.png'
 import More from '../img/more.png'
+import Messages from './Messages'
 
 const Chat = () => {
   return (
@@ -13,6 +14,7 @@ const Chat = () => {
           <img src={Add} alt="" />
           <img src={More} alt="" />
         </div>
+        <Messages />
       </div>
     </div>
   )
