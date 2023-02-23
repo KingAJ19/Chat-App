27f8ec5beff1132c11a0f1776feddd5f87e742e4
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 11:59:46 +0200

    commented

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -3,6 +3,37 @@ import React from 'react'
 const Chats = () => {
   return (
     <div className='chats'>
+      {/* User Starts */}
+      <div className="userChat">
+        <img className='searchImage' src="https://resources.premierleague.com/premierleague/photo/2023/01/04/effad1fa-ede8-4a56-83d4-d2c840bddf44/Kane-Spurs.jpg" alt="" />
+        <div className="userChatsInfo">
+          <span>Harry Kane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      {/* User Ends */}
+      <div className="userChat">
+        <img className='searchImage' src="https://resources.premierleague.com/premierleague/photo/2023/01/04/effad1fa-ede8-4a56-83d4-d2c840bddf44/Kane-Spurs.jpg" alt="" />
+        <div className="userChatsInfo">
+          <span>Harry Kane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img className='searchImage' src="https://resources.premierleague.com/premierleague/photo/2023/01/04/effad1fa-ede8-4a56-83d4-d2c840bddf44/Kane-Spurs.jpg" alt="" />
+        <div className="userChatsInfo">
+          <span>Harry Kane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+
+      <div className="userChat">
+        <img className='searchImage' src="https://resources.premierleague.com/premierleague/photo/2023/01/04/effad1fa-ede8-4a56-83d4-d2c840bddf44/Kane-Spurs.jpg" alt="" />
+        <div className="userChatsInfo">
+          <span>Harry Kane</span>
+          <p>Hello</p>
+        </div>
+      </div>
       <div className="userChat">
         <img className='searchImage' src="https://resources.premierleague.com/premierleague/photo/2023/01/04/effad1fa-ede8-4a56-83d4-d2c840bddf44/Kane-Spurs.jpg" alt="" />
         <div className="userChatsInfo">
