22e9eb01c20dddf236afd94a87682b89ec95acc8
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-23 12:01:07 +0200

    salah

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -13,9 +13,9 @@ const Chats = () => {
       </div>
       {/* User Ends */}
       <div className="userChat">
-        <img className='searchImage' src="https://resources.premierleague.com/premierleague/photo/2023/01/04/effad1fa-ede8-4a56-83d4-d2c840bddf44/Kane-Spurs.jpg" alt="" />
+        <img className='searchImage' src="https://e0.365dm.com/22/01/2048x1152/skysports-mohamed-salah-liverpool_5638215.jpg" alt="" />
         <div className="userChatsInfo">
-          <span>Harry Kane</span>
+          <span>Mo Salah</span>
           <p>Hello</p>
         </div>
       </div>
