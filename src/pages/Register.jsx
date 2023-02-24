c28f50b7584ee20564861500a04fd42fb895e1c0
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-24 03:22:44 +0200

    async

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,27 +3,19 @@ import '../responsive.css'
 import Add from "../img/addAvatar.png"
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase'
+import { async } from '@firebase/util';
 
 export const Register = () => {
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
     const file = e.target[3].files[0];
 
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      //Signed in
-      const user = userCredential.user;
-      console.log(user)
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      //
-    })
+    const res = await createUserWithEmailAndPassword(auth, email, password)
+    
   }
 
 
