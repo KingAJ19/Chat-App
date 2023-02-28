74c495bc9855ad322118f1b6a1f14c185164262c
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-28 08:37:55 +0200

    oi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { Register } from "./pages/Register";
-
 import './style.css'
 
 function App() {
