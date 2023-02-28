97768e6a53ce09ce7745fef55da92c4390873aa0
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-28 09:40:07 +0200

    tag

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/"> 
-          <Route index element={<Home />}
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
