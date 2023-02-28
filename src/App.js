83831dfe413d0da56a885a9b11beb38f53558f2b
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-28 08:37:17 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { Register } from "./pages/Register";
+
 import './style.css'
 
 function App() {
