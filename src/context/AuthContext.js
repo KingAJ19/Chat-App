dae39f9ea926dced9147213a67a8a6a0c3a2a83a
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-03-06 10:36:40 +0200

    oi oi

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,5 +4,5 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState()
+    const [currentUser, setCurrentUser] = useState({})
 }
