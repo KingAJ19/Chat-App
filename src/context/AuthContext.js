16ec5503ce8f367a590c59160adefed65b9e224f
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-03-08 04:08:34 +0200

    changes

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,25 +6,25 @@ import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
-export const AuthContextProvider = ({children}) => {
+export const AuthContextProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState({})
 
     useEffect(() => {
-       const unsub = onAuthStateChanged(auth, (user) => {
+        const unsub = onAuthStateChanged(auth, (user) => {
             setCurrentUser(user);
             console.log(user)
         });
 
-        return () => {   
+        return () => {
             unsub();
         }
 
     }, []);
 
     return (
-        <AuthContext.Provider value={{currentUser}}>
-           {children}
+        <AuthContext.Provider value={{ currentUser }}>
+            {children}
         </AuthContext.Provider>
     )
-    
+
 };
