2032503dba8b5faf494f9fda3e5cee331cb7bc2b
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-03-24 03:58:29 +0200

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { auth } from '../firebase'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <span className="navLogo">Super Chat</span>
+      <span className="navLogo">Super Chat</span> 
       <div className="user">
         <img className='navImage' src="https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt67ae3fa7e0798570/63dc178bb19823132904002b/Darwin_Nunez_cry_gesture_Liverpool_2022-23.png" alt="" />
         <span>John</span>
