4f1d04863d100724fbb02182e5322c60e20f1ce8
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-04-11 06:05:54 +0200

    oi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
       <div className="user">
         <img className='navImage' src="https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt67ae3fa7e0798570/63dc178bb19823132904002b/Darwin_Nunez_cry_gesture_Liverpool_2022-23.png" alt="" />
         <span>John</span>
-        <button className='navBtn' onClick={() => signOut(auth)}>Log out</button>
+        <button className='navBtn' onClick={() => signOut(auth)}>Log out</button> 
       </div>
     </div>
   )
